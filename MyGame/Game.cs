ed341219f7d931197973e950df2638ede8b8afbb
Kirill Palenov <kpalenov@mail.ru>
2018-12-04 01:36:03 +0300

    MyGame Project Initialization

---
+++ MyGame/Game.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyGame
+{
+    static class Game
+    {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;
+        public static BaseObject[] _objs;
+        // Свойства
+        // Ширина и высота игрового поля
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+
+        static Game()
+        {
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
+        public static void Init(Form form)
+        {
+            // Графическое устройство для вывода графики            
+            Graphics g;
+            // Предоставляет доступ к главному буферу графического контекста для текущего приложения
+            _context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();
+            // Создаем объект (поверхность рисования) и связываем его с формой
+            // Запоминаем размеры формы
+            Width = form.ClientSize.Width;
+            Height = form.ClientSize.Height;
+            // Связываем буфер в памяти с графическим объектом, чтобы рисовать в буфере
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+            Load();
+
+            Timer timer = new Timer { Interval = 300 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
+        }
+
+        public static void Draw()
+        {
+            // Проверяем вывод графики
+            Buffer.Graphics.Clear(Color.Black);
+            //Buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+            //Buffer.Graphics.FillEllipse(Brushes.Blue, new Rectangle(100, 100, 200, 200));
+            Buffer.Render();
+
+            Buffer.Graphics.Clear(Color.Black);
+            foreach (BaseObject obj in _objs)
+                obj.Draw();
+            Buffer.Render();
+        }
+
+        public static void Load()
+        {
+            _objs = new BaseObject[30];
+            for (int i = 0; i < _objs.Length / 2 - 1; i++)
+                _objs[i] = new Cat(new Point(600, i * 20), new Point(-i, -i), new Size(10, 10));
+            _objs[_objs.Length / 2 - 1] = new Mars(new Point(200, 100), new Point(200, 50), new Size(100, 100));
+            for (int i = _objs.Length / 2; i < _objs.Length - 1; i++)
+                _objs[i] = new Star(new Point(600, i * 20), new Point(-i, 0), new Size(5, 5));
+            _objs[_objs.Length - 1] = new Ellipse(new Point(600, 300), new Point(-100, 0), new Size(100, 100));
+        }
+
+        public static void Update()
+        {
+            foreach (BaseObject obj in _objs)
+                obj.Update();
+        }
+    }
+}
---
+++ MyGame/Mars.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    class Mars : BaseObject
+    {
+        public Mars(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+
+        public override void Draw()
+        {
+            Image newImage = Image.FromFile("Mars.jpg");
+            Game.Buffer.Graphics.DrawImage(newImage, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+
+        public override void Update()
+        {
+            Pos.X = Pos.X + Dir.X; //поменял на +, как и было рекомедовано в комментарии
+            if (Pos.X > Game.Width) Pos.X = 0 + Size.Width;
+        }
+    }
+}
